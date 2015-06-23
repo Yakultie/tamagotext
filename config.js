a2dd6fadf5de6a9bf4dab3cfee281999d468cacd
Yakultie <Yakultie@iCloud.com>
2015-06-23 19:11:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.port = process.env.PORT || 8000;
 
 // This should be your own mobile telephone number
 // You will use it to control your Tamagotchi
-config.myNumber = process.env.MY_NUMBER;
+config.myNumber = process.env.+85291084278;
 
 // Your Twilio account SID and auth token, both found at:
 // https://www.twilio.com/user/account
@@ -14,16 +14,16 @@ config.myNumber = process.env.MY_NUMBER;
 config.twilioConfig = {
     // You can get your AccountSid and AuthToken from https://www.twilio.com/user/account/voice
     // Make sure you expand the Show API Credentials
-    accountSid: process.env.TWILIO_ACCOUNT_SID,
-    authToken: process.env.TWILIO_AUTH_TOKEN,
+    accountSid: process.env.AC586c6e3caac0c42d221eeedd91c14ed3,
+    authToken: process.env.d515c34e67c95a1dddff602cc016af44,
     // A Twilio number you control - choose one from:
     // https://www.twilio.com/user/account/phone-numbers/incoming
-    number: process.env.TWILIO_NUMBER
+    number: process.env.+85264522488
   }
   // configure foods and games to be played
 config.actions = {
-    pet: ["hangman", "tic-tac-toe", "human knot", "london bridges", "frog races", "water balloon fights", "silly relay races"],
-    food: ["apple", "cheetos", "burrito", "lettuce", "pear", "ice cream"]
+    pet: ["hangman", "tic-tac-toe", "human knot", "london bridges", "frog races", "water balloon fights", "silly relay races", "Far Cry 4", "GTA5"],
+    food: ["apple", "cheetos", "burrito", "lettuce", "pear", "ice cream", "froyo", "chicken nugget", "chicken wing"]
   }
   // Export configuration object
 module.exports = config;
